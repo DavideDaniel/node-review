5b22803d316363cbb8d7bc6145cf0a7eab9238a6
DavideDaniel <davidedaniel@gmail.com>
2015-10-29 08:22:34 -0400

    first commit

---
+++ simpleHttpServer.js
@@ -0,0 +1,11 @@
+'use strict';
+const http = require('http'); // constant because we want http to be unique
+
+http
+  .createServer((req, res) => { // new syntax for anonymous function
+    res.writeHead(200, {
+      'Content-type': 'text/html'
+    });
+    res.end('<h1>Hello NodeJS</h1>');
+  })
+  .listen(3000, () => console.log('Server running on port 3000'));
